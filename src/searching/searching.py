17443326ca7ceb2c7a799466d6d78f34ff139d54
Latosha Tims <ltims080913@hotmail.com>
2020-06-16 12:28:18 -0700

    finished searching mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for x in range(0, len(arr)):
+        if arr[x] == target:
+            return x
 
 
     return -1   # not found
@@ -9,6 +12,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    lower = 0
+    higher = len(arr)
+
+    while lower < higher:
+        middle = (lower + higher) // 2
+        if arr[middle] == target:
+            return middle
+        elif target > arr[middle]:
+            lower = middle
+        else:
+            higher = middle + 1
+
 
 
     return -1  # not found
